1aeca0f5bfb3e829c8d532c20dd76c9bb4f7e5ef
lindexi <lindexi_gd@163.com>
2025-10-31 16:23:25 +0800

    尝试实现但失败

---
+++ src/DotNetCampus.AvaloniaInkCanvas/StrokeRenderers/SimpleSkiaInkStrokeRenderer.cs
@@ -0,0 +1,13 @@
+﻿using DotNetCampus.Inking.Primitive;
+using SkiaSharp;
+
+namespace DotNetCampus.Inking.StrokeRenderers;
+
+class SimpleSkiaInkStrokeRenderer : ISkiaInkStrokeRenderer
+{
+    private readonly SkiaSimpleInkRender _skiaSimpleInkRender = new SkiaSimpleInkRender();
+
+    public SKPath RenderInkToPath(IReadOnlyList<InkStylusPoint> pointList, double inkThickness)
+    {
+    }
+}
